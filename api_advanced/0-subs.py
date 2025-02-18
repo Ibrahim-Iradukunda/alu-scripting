85a26160b4928bd6447f08a958a54c43d6b41934
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-18 20:23:04 +0200

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
+"""DOC"""
 import requests
 
-def number_of_subscribers(subreddit):
-    # Define the base URL for the Reddit API
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Define custom User-Agent to avoid rate limiting issues
-    headers = {
-        'User-Agent': 'Mozilla/5.0'
-    }
-    
-    try:
-        # Send a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the response status code is 200 (OK)
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            # Extract the number of subscribers
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
-        return 0
 
+def number_of_subscribers(subreddit):
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
+    return 0
