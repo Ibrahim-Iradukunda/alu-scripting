0190795500cdbc1f0fd565f820f70b06a5920aa0
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 10:23:01 +0200

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,9 +2,8 @@
 """Contains top_ten function"""
 import requests
 
-
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """Print the posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
