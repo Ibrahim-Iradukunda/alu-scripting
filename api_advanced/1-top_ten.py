297d8d1351cdf0644e4f9b7d170a02575bcf475e
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 10:41:47 +0200

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,13 +2,11 @@
 """Contains top_ten function"""
 import requests
 
-
-
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "limit": 10
@@ -20,10 +18,13 @@ def top_ten(subreddit):
             return
         results = response.json().get("data")
         if results:
-            [print(c.get("data").get("title")) for c in results.get("children")]
+            for c in results.get("children"):
+                print(c.get("data").get("title"))
+            print("OK")
         else:
             print("None")
     except requests.RequestException:
         print("None")
     except ValueError:
         print("None")
+
