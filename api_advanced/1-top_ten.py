05bc9c79f5c8b6584f94c68811567f5513623389
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 11:12:14 +0200

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,11 +7,15 @@ def top_ten(subreddit):
     """Docs"""
     reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        data = response.json().get('data', {})
+        if data:
+            for post in data.get('children', [])[:10]:
+                print(post['data']['title'])
+        else:
+            print("None")
     else:
         print("None")
+
