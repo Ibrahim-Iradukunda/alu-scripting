db982b9cad591e28923f37c9b77cdf5ecc3b1d49
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 09:52:32 +0200

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+1-top_ten module
+"""
 import requests
 
 def top_ten(subreddit):
+    """Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
     
@@ -13,5 +17,5 @@ def top_ten(subreddit):
                 print(post.get("data", {}).get("title", "None"))
         else:
             print(None)
-    except Exception as e:
+    except requests.RequestException:
         print(None)
