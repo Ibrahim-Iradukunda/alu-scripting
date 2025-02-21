5a9b81fa3b2775df641c46f6590953193f72f02f
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 22:18:42 +0200

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,8 +4,9 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """ prints the titles of the first 10 hot posts """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+    .format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
