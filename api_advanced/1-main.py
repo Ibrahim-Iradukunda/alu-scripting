3c663bbf1c308227639941aeef4cf5f04f2cc29a
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 10:37:27 +0200

    updated

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -7,6 +7,6 @@ from 1-top_ten import top_ten
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit.")
+        print("Please pass an argument for the subreddit to search.")
     else:
         top_ten(sys.argv[1])
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,17 +3,16 @@
 import requests
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "limit": 10
     }
     try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
         if response.status_code == 404:
             print("None")
             return
