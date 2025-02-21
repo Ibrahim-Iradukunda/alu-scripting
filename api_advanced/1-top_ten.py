60f3374996416562db75f2deff5c24cac69a85ca
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 11:00:34 +0200

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
-
+"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-
-        # Check if the request was successful
-        if RESPONSE.status_code != 200:
-            print("None")
-            return
-
-        DATA = RESPONSE.json().get("data")
-
-        # Check if 'data' exists and has 'children'
-        if not DATA or "children" not in DATA:
-            print("None")
-            return
-
-        HOT_POSTS = DATA["children"]
-
-        # Print the top 10 post titles
-        for post in HOT_POSTS:
-            print(post["data"].get("title", "None"))
-
-    except Exception:
-        print("None")
-
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
