052ecb6c1ce5396a160811ab862eab3f2e17dce4
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-02-21 10:45:45 +0200

    updated

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -2,12 +2,12 @@
 """
 1-main
 """
-import sys
-from 1-top_ten import top_ten
 
-if __name__ == '__main__':
+import sys
+
+if __name__ == '__main__'
+    top_ten = __import__('1-top_ten').top_ten
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
         top_ten(sys.argv[1])
-
