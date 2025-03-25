c34e1fee5896651829b2af5a16af97819e239cb7
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-03-25 18:27:27 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,30 +4,14 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print titles of top 10 hot posts for a subreddit or 'OK' when needed."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    """Print titles of top 10 hot posts for a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post.get('data', {}).get('title', '').strip())
-            else:
-                # Valid subreddit, but no posts
-                print("OK")
-        else:
-            # Invalid subreddit
-            print("OK")
-    except Exception:
-        # Handle errors or unexpected issues
-        print("OK")
-
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        top_ten(sys.argv[1])
-    else:
-        print("Please pass an argument for the subreddit to search.")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None, end='')
+        return
+    posts = response.json().get('data', {}).get('children', [])
+    for i in range(min(10, len(posts))):
+        print(posts[i].get('data', {}).get('title', '').strip())
