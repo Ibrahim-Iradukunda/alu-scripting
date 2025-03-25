7944e6944e0b62d13def4c5c60139bef5f247726
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-03-25 17:50:37 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
-        print(None)
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    """Print titles of top 10 hot posts for a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None, end='')
+        return
+    posts = response.json().get('data', {}).get('children', [])
+    for i in range(min(10, len(posts))):
+        print(posts[i].get('data', {}).get('title', '').strip())
