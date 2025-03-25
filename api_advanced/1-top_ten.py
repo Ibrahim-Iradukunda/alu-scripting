fb5a6c35f3c36a96b74ebe2e3dc37a4a1bc6038c
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-03-25 18:08:13 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,14 +11,21 @@ def top_ten(subreddit):
         returns the top ten posts for a given subreddit
     '''
     user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
+        response = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                                .format(subreddit), headers=user)
+        if response.status_code != 200:
+            print(None)
+            return
+        data = response.json()
+        for post in data.get('data', {}).get('children', []):
+            print(post.get('data', {}).get('title'))
     except Exception:
         print(None)
 
 
 if __name__ == "__main__":
-    top_ten(argv[1])
+    if len(argv) > 1:
+        top_ten(argv[1])
+    else:
+        print(None)
