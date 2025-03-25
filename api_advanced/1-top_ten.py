f7cf6c40b5cce5dfb1273ad8c8ada85079312455
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2025-03-25 18:36:28 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Get the titles of the first 10 hot posts for a given subreddit."""
+"""
+Module to query the Reddit API and print the titles
+of the first 10 hot posts listed for a given subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print titles of top 10 hot posts for a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    """
+    Queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit
+    """
+    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
 
-    # Make the request and prevent redirects
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check for non-200 status codes (e.g., invalid subreddit)
-    if response.status_code != 200:
-        print(None)
-        return
-
-    # Extract posts data
-    posts = response.json().get('data', {}).get('children', [])
-    if not posts:
-        # Handle case where subreddit exists but has no posts
-        print(None)
-        return
-
-    # Print the titles of the top 10 hot posts
-    for i in range(min(10, len(posts))):
-        print(posts[i].get('data', {}).get('title', '').strip())
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        if data and 'children' in data:
+            for post in data['children'][:10]:
+                print(post['data']['title'])
+        else:
+            print("None")
+    else:
+        print("None")
